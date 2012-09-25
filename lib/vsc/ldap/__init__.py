7cfa61f3a96f81cd2ce09c6d47fb818e00c05919
Andy Georges <itkovian@gmail.com>
2012-09-25 17:18:02 +0200

    Updated header.

--- lib/vsc/ldap/__init__.py
+++ lib/vsc/ldap/__init__.py
@@ -9,6 +9,15 @@
 # This is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation v2.
+#
+# VSC-tools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with VSC-tools. If not, see <http://www.gnu.org/licenses/>.
+##
 """vsc.ldap package.
 
 Provides:
