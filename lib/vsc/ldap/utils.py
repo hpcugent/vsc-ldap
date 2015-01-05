72098eac265b835f98aee0bdba56b8175b21bed4
Andy Georges <itkovian@gmail.com>
2015-01-05 12:42:39 +0100

    The modlist is incorrect when gecos was empty, fix this using fake exisitng gecos

--- lib/vsc/ldap/utils.py
+++ lib/vsc/ldap/utils.py
@@ -491,6 +491,9 @@ class LdapQuery(object):
         current_ = {}
         for key in attributes.keys():
             current_[key] = current.get(key, [])
+            if current_[key] is '':
+                self.log.warning("Replacing empty string for key %s with foo before making modlist for dn %s" % (key, dn))
+                current_[key] = "foo"  # hack to allow replacing empty strings
         # [(ldap.MOD_REPLACE, k, v) for (k,v) in attributes.iteritems()]
         modification_attributes = ldap.modlist.modifyModlist(current_, attributes)
 
